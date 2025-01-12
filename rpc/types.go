20d69ae41c455b1db80cf5625ef15e8226b478dd
r. ávila <avila.dev@outlook.com>
2025-01-12 15:18:49 -0300

    feat: implement initial types & constants

---
+++ rpc/types.go
@@ -0,0 +1,35 @@
+package rpc
+
+import (
+	"github.com/avila-r/bitclient/config"
+)
+
+type (
+	Version string
+	Method  string
+	ID      string
+	Params  []any
+	Header  string
+)
+
+const (
+	Version2 Version = "2.0"
+
+	ContentTypeHeaderLabel   = "Content-Type"
+	AuthorizationHeaderLabel = "Authorization"
+
+	MethodGetBlock          Method = "getblock"
+	MethodGetBlockchainInfo Method = "getblockchaininfo"
+
+	ContentTypeApplicationJson Header = "application/json"
+)
+
+var (
+	Identifier ID = ID(config.Get().Main.Use)
+
+	NoParams Params = Params{}
+
+	Bearer = func(token string) string {
+		return "Bearer " + token
+	}
+)
