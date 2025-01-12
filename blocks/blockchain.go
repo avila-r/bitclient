39d376d70640880119de255614086f1b53d611a1
r. ávila <avila.dev@outlook.com>
2025-01-12 16:58:28 -0300

    feat: implement method to get blockchain info

---
+++ blocks/blockchain.go
@@ -0,0 +1,16 @@
+package blocks
+
+import (
+	"github.com/avila-r/bitclient/rpc"
+)
+
+func GetBlockchainInfo() (*rpc.Json, error) {
+	request := rpc.Request{
+		ID:      rpc.Identifier,
+		Version: rpc.Version2,
+		Method:  rpc.MethodGetBlockchainInfo,
+		Params:  rpc.NoParams,
+	}
+
+	return rpc.DefaultClient.Do(request)
+}
