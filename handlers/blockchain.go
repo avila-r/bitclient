cc48e251e07b715a03f20cb35da785ff67c99fff
r. ávila <avila.dev@outlook.com>
2025-01-12 16:58:50 -0300

    feat: transfer responsability to unwrap rpc request to bitcliclient/blocks package

--- handlers/blockchain.go
+++ handlers/blockchain.go
@@ -3,25 +3,18 @@ package handlers
 import (
 	"github.com/spf13/cobra"
 
+	"github.com/avila-r/bitclient/blocks"
 	"github.com/avila-r/bitclient/logger"
-	"github.com/avila-r/bitclient/rpc"
 )
 
-type blockchain Handler
+type blockchainHandler Handler
 
 // There's no handler for
 // default 'blockchain' command
-var Blockchain blockchain = nil
+var Blockchain blockchainHandler = nil
 
-func (h *blockchain) Info(cmd *cobra.Command, args []string) {
-	request := rpc.Request{
-		ID:      rpc.Identifier,
-		Version: rpc.Version2,
-		Method:  rpc.MethodGetBlockchainInfo,
-		Params:  rpc.NoParams,
-	}
-
-	response, err := rpc.DefaultClient.Do(request)
+func (h *blockchainHandler) Info(cmd *cobra.Command, args []string) {
+	response, err := blocks.GetBlockchainInfo()
 	if err != nil {
 		logger.Errorf("failed to get blockchain info: %v", err.Error())
 		return
