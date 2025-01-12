7ccd6624af0cdb1d913b0f3dbbe9d2a5008df5f0
r. ávila <avila.dev@outlook.com>
2025-01-12 20:43:45 -0300

    feat: implement getblock procedure call

---
+++ blocks/get.go
@@ -0,0 +1,44 @@
+package blocks
+
+import (
+	"regexp"
+
+	"github.com/avila-r/bitclient/errs"
+	"github.com/avila-r/bitclient/rpc"
+)
+
+type ResponseVerbosity int
+
+const (
+	VerbositySerializedHexData ResponseVerbosity = iota
+	VerbosityBasicBlockInfo
+	VerbosityDetailedBlockInfo
+	VerbosityFullBlockInfoWithPrevout
+)
+
+func VerbosityFrom(i int) (ResponseVerbosity, error) {
+	if i < int(VerbositySerializedHexData) || i > int(VerbosityFullBlockInfoWithPrevout) {
+		return 0, errs.Of("invalid verbosity level: %d, valid range is 0-3", i)
+	}
+	return ResponseVerbosity(i), nil
+}
+
+func GetBlock(blockhash string, verbosity int) (*rpc.Json, error) {
+	if len(blockhash) != 64 || !regexp.MustCompile("^[0-9a-fA-F]{64}$").MatchString(blockhash) {
+		return nil, errs.Of("blockhash provided isn't in valid format")
+	}
+
+	_, err := VerbosityFrom(verbosity)
+	if err != nil {
+		return nil, err
+	}
+
+	request := rpc.Request{
+		ID:      rpc.Identifier,
+		Version: rpc.Version2,
+		Method:  rpc.MethodGetBlock,
+		Params:  rpc.Params{blockhash, verbosity},
+	}
+
+	return Result(rpc.Client.Do(request))
+}
