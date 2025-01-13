6638e25c952baefd072d5fb92d9e4f8623734fa1
r. ávila <avila.dev@outlook.com>
2025-01-13 18:02:54 -0300

    feat: implement calls

--- blocks/blockchain.go
+++ blocks/blockchain.go
@@ -8,9 +8,31 @@ func GetBlockchainInfo() (*rpc.Json, error) {
 	request := rpc.Request{
 		ID:      rpc.Identifier,
 		Version: rpc.Version2,
-		Method:  rpc.MethodGetBlockchainInfo,
+		Method:  MethodGetBlockchainInfo,
 		Params:  rpc.NoParams,
 	}
 
 	return rpc.JsonResult(rpc.Client.Do(request))
+}
+
+func GetBlockCount() (*rpc.Response, error) {
+	request := rpc.Request{
+		ID:      rpc.Identifier,
+		Version: rpc.Version2,
+		Method:  MethodGetBlockCount,
+		Params:  rpc.NoParams,
+	}
+
+	return rpc.Client.Do(request)
+}
+
+func GetBestBlockHash() (*rpc.Response, error) {
+	request := rpc.Request{
+		ID:      rpc.Identifier,
+		Version: rpc.Version2,
+		Method:  MethodGetBestBlockHash,
+		Params:  rpc.NoParams,
+	}
+
+	return rpc.Client.Do(request)
 }
