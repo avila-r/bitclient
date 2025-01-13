363f1e4fc0f296073dd43d9cda3b6602bdc03e4a
r. ávila <avila.dev@outlook.com>
2025-01-13 13:04:21 -0300

    fix: refactor usages

--- blocks/blocks.go
+++ blocks/blocks.go
@@ -18,12 +18,12 @@ const (
 
 func VerbosityFrom(i int) (ResponseVerbosity, error) {
 	if i < int(VerbositySerializedHexData) || i > int(VerbosityFullBlockInfoWithPrevout) {
-		return 0, errs.Of("invalid verbosity level: %d, valid range is 0-3", i)
+		return 0, errs.Of("invalid verbosity level (%d), valid range is 0-3", i)
 	}
 	return ResponseVerbosity(i), nil
 }
 
-func GetBlock(blockhash string, verbosity int) (*rpc.Json, error) {
+func GetBlock(blockhash string, verbosity int) (*rpc.Response, error) {
 	if len(blockhash) != 64 || !regexp.MustCompile("^[0-9a-fA-F]{64}$").MatchString(blockhash) {
 		return nil, errs.Of("blockhash provided isn't in valid format")
 	}
@@ -40,5 +40,5 @@ func GetBlock(blockhash string, verbosity int) (*rpc.Json, error) {
 		Params:  rpc.Params{blockhash, verbosity},
 	}
 
-	return Result(rpc.Client.Do(request))
+	return rpc.Client.Do(request)
 }
