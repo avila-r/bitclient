baab15f0def6d4dfebed9dcf906fabaa8365a0c9
r. ávila <avila.dev@outlook.com>
2025-01-14 12:08:54 -0300

    docs: set up inline comments

--- blocks/methods.go
+++ blocks/methods.go
@@ -3,13 +3,13 @@ package blocks
 import "github.com/avila-r/bitclient/rpc"
 
 const (
-	MethodGetBestBlockHash  rpc.Method = "getbestblockhash"
-	MethodGetBlock          rpc.Method = "getblock"
-	MethodGetBlockchainInfo rpc.Method = "getblockchaininfo"
-	MethodGetBlockCount     rpc.Method = "getblockcount"
-	MethodGetBlockFilter    rpc.Method = "getblockfilter"
-	MethodGetBlockHash      rpc.Method = "getblockhash"
-	MethodGetBlockHeader    rpc.Method = "getblockheader"
-	MethodGetBlockStats     rpc.Method = "getblockstats"
-	MethodGetChainTips      rpc.Method = "getchaintips"
+	MethodGetBestBlockHash  rpc.Method = "getbestblockhash"  // Method to get the best block hash
+	MethodGetBlock          rpc.Method = "getblock"          // Method to get block data by hash
+	MethodGetBlockchainInfo rpc.Method = "getblockchaininfo" // Method to get blockchain info
+	MethodGetBlockCount     rpc.Method = "getblockcount"     // Method to get the block count
+	MethodGetBlockFilter    rpc.Method = "getblockfilter"    // Method to get a block filter by block hash
+	MethodGetBlockHash      rpc.Method = "getblockhash"      // Method to get block hash by height
+	MethodGetBlockHeader    rpc.Method = "getblockheader"    // Method to get block header by hash
+	MethodGetBlockStats     rpc.Method = "getblockstats"     // Method to get block stats by hash or height
+	MethodGetChainTips      rpc.Method = "getchaintips"      // Method to get chain stats
 )
