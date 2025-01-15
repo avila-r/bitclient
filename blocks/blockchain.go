093e529711e1c1bea2501a08603df3d1d420cd39
r. ávila <avila.dev@outlook.com>
2025-01-14 23:13:41 -0300

    feat: implement procedure calls

--- blocks/blockchain.go
+++ blocks/blockchain.go
@@ -1,6 +1,8 @@
 package blocks
 
 import (
+	"math/big"
+
 	"github.com/avila-r/bitclient/rpc"
 )
 
@@ -156,4 +158,89 @@ func GetChainTips() (*rpc.Array, error) {
 	}
 
 	return rpc.ArrayResult(rpc.Client.Do(request))
+}
+
+// GetChainTxStats retrieves the transaction statistics for a given chain of blocks.
+//
+// This function sends a JSON-RPC request to the Bitcoin client using the "getchaintxstats" procedure call.
+// The response provides transaction statistics for the chain, including the number of transactions, the time span, and other metrics.
+//
+// Returns:
+// - *rpc.Json: The JSON-RPC response containing transaction statistics for the chain in a structured format.
+// - error: An error if the request fails or if there is an issue with the response.
+//
+// Example Usage:
+//
+//   - Using Bitclient:
+//     $ bitclient blockchain txstats
+//
+//   - Using the Bitcoin CLI:
+//     $ bitcoin-cli getchaintxstats
+//
+//   - Using cURL:
+//     $ curl --user {username} --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "getchaintxstats", "params": [nblocks, blockhash]}' \
+//     -H 'content-type: text/plain;' {url}
+//
+// Note:
+// Ensure the RPC client is properly configured and connected to the Bitcoin node before calling this function.
+// The node must be synchronized for accurate transaction statistics.
+func GetChainTxStats(nblocks int, blockhash ...string) (*rpc.Json, error) {
+	params := rpc.Params{}
+	if nblocks > 0 {
+		params = append(params, nblocks)
+	}
+	if len(blockhash) > 0 {
+		params = append(params, blockhash)
+	}
+
+	request := rpc.Request{
+		ID:      rpc.Identifier,
+		Version: rpc.Version2,
+		Method:  MethodGetChainTxStats,
+		Params:  params,
+	}
+
+	return rpc.JsonResult(rpc.Client.Do(request))
+}
+
+// GetDifficulty retrieves the current mining difficulty of the Bitcoin network.
+//
+// This function sends a JSON-RPC request to the Bitcoin client using the "getdifficulty" procedure call.
+// The response provides the current difficulty, which is a measure of how difficult it is to find a new block.
+//
+// Returns:
+// - *big.Float: The current mining difficulty as a floating-point number.
+// - error: An error if the request fails or if there is an issue with the response.
+//
+// Example Usage:
+//
+//   - Using Bitclient:
+//     $ bitclient blockchain difficulty
+//
+//   - Using the Bitcoin CLI:
+//     $ bitcoin-cli getdifficulty
+//
+//   - Using cURL:
+//     $ curl --user {username} --data-binary '{"jsonrpc": "1.0", "id": "curltest", "method": "getdifficulty", "params": []}' \
+//     -H 'content-type: text/plain;' {url}
+//
+// Note:
+// Ensure the RPC client is properly configured and connected to the Bitcoin node before calling this function.
+// The node must be synchronized to return an accurate difficulty value.
+func GetDifficulty() (*big.Float, error) {
+	request := rpc.Request{
+		ID:      rpc.Identifier,
+		Version: rpc.Version2,
+		Method:  MethodGetDifficulty,
+		Params:  rpc.NoParams,
+	}
+
+	response, err := rpc.Client.Do(request)
+	if response == nil || err != nil {
+		return nil, err
+	}
+
+	var difficulty big.Float
+	difficulty.SetString(string(response.Result))
+	return &difficulty, nil
 }
--- blocks/methods.go
+++ blocks/methods.go
@@ -11,5 +11,7 @@ const (
 	MethodGetBlockHash      rpc.Method = "getblockhash"      // Method to get block hash by height
 	MethodGetBlockHeader    rpc.Method = "getblockheader"    // Method to get block header by hash
 	MethodGetBlockStats     rpc.Method = "getblockstats"     // Method to get block stats by hash or height
-	MethodGetChainTips      rpc.Method = "getchaintips"      // Method to get chain stats
+	MethodGetChainTips      rpc.Method = "getchaintips"      // Method to get chain tips
+	MethodGetChainTxStats   rpc.Method = "getchaintxstats"   // Method to get chain transaction stats
+	MethodGetDifficulty     rpc.Method = "getdifficulty"     // Method to get the current mining difficulty
 )
